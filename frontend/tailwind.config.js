75700386e7dfb930c9f24dc34f572cf1b1383bd5
Koushik <Koushikbajpayee06@gmail.com>
2025-11-26 21:14:59 +0530

    top doctors to book features compleated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,10 @@ export default {
   theme: {
     extend: {
       colors:{
-        'primary':"#5f6FFF"
+        'primary':"#5f6FFF",
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill, minmax(200px,1fr))'
       }
     },
   },
