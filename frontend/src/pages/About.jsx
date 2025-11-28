65267eb9f549f53a0a3b8d0a95e1cd957234cbff
Koushik <Koushikbajpayee06@gmail.com>
2025-11-28 18:09:41 +0530

    contact us page created successfully

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <div className='text-center text-2xl pt-10 text-gray-500'>
-        <p>ABOUT <span className='text-gray-700 font-medium'>Us</span></p>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
       </div>    
         <div className='my-10 flex flex-col md:flex-row gap-12'>
           <img className='w-full md:max-w-[360px]' src={assets.about_image} alt=''/>
