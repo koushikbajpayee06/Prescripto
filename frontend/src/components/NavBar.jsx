0163f6eb93897deb57444da3f6d2bc22822f64ad
Koushik <Koushikbajpayee06@gmail.com>
2025-11-28 12:19:11 +0530

    doctors page is ready

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,15 +5,15 @@ import { NavLink, useNavigate } from 'react-router-dom'
 const NavBar = () => {
     const navigate = useNavigate();
 
-    const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);  
 
     const [token,setToken] = useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
-            <li className='py-1'>Home</li>
+            <li className='py-1'>HOME</li>
             <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
         <NavLink to='/doctors'>
