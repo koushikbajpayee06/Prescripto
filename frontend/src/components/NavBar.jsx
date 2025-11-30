f9c32166fdb19c3708f5a0661534d2824ee15f96
Koushik <Koushikbajpayee06@gmail.com>
2025-11-30 11:00:08 +0530

    profile page created

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
                 <img className='w-2.5' src={assets.dropdown_icon} alt=''/>
                 <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                        <p onClick={()=>navigate('/profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                        <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                         <p onClick={()=>navigate('my-appoinments')} className='hover:text-black cursor-pointer'>My Appoinments</p>
                         <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
                     </div>
