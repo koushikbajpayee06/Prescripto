6c149e510f5732a6477ff2090d40ffb9348ba0a7
Koushik <Koushikbajpayee06@gmail.com>
2025-12-01 19:05:45 +0530

    add multer upload config using diskStorage and export upload middleware

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,12 @@
+
+
+// API for adding doctor
+const addDoctor = async (req,res)=>{
+    try{
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+    }catch(erroe){
+
+    }
+}
+
+export {addDoctor}
