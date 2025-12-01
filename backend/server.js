d19e6da726313e7fbaa298b379bb2ac549c34a8b
Koushik <Koushikbajpayee06@gmail.com>
2025-12-01 15:04:45 +0530

    database connection created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
 
 // app config 
 const app = express()
 const port = process.env.PORT ||  4000;
+connectDB()
+    .then(() => {
+        console.log("Database connection established...");
+        app.listen(port, () => {
+            console.log("server is successful listening on",port);
+        });
+    })
+    .catch(err => {
+        console.error("Database cant be connected");
+    });
+
 
 // middlewares
 app.use(express.json())
@@ -15,4 +28,3 @@ app.get('/abc',(req, res)=>{
     res.send("API working")
 });
 
-app.listen(port,()=> console.log("Server Started", port)); 
