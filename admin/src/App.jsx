fa67bbb5cdd73c419ff2c03573a2f382f0fd5456
Koushik <Koushikbajpayee06@gmail.com>
2025-12-22 14:52:08 +0530

    Login & logout implemented successfully

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/adminContext';
+import NavBar from './components/NavBar';
 
 const App = () => {
-  return (
+  const {aToken} = useContext(AdminContext);
+
+  return aToken ? (
+    <div className='bg-[#F8F9FD]'>
+      <ToastContainer />
+      <NavBar/>
+    </div>
+  ): (
     <div>
       <Login/>
       <ToastContainer position="top-right" autoClose={2000}/>
     </div>
-  )
+  ) 
 }
 
 export default App;
