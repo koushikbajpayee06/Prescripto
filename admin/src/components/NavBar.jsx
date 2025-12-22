21edf876e2161a2f1858a33cd60eb7377404fe7c
Koushik <Koushikbajpayee06@gmail.com>
2025-12-22 17:56:55 +0530

    side bar added in admin panel page

--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AdminContext } from '../context/adminContext'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
     const {aToken, setAToken} = useContext(AdminContext);
+    const navigate = useNavigate()
 
     const logout = ()=>{
+        navigate('/');
         aToken && setAToken('');
         aToken  && localStorage.removeItem("aToken");
     }
---
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddDoctor = () => {
+  return (
+    <div>
+      ADD Doctors
+    </div>
+  )
+}
+
+export default AddDoctor
