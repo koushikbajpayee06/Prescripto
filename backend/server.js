ecb2b5a464a595e385519e3bbb330dcbe6c22907
Koushik <Koushikbajpayee06@gmail.com>
2025-12-24 23:26:58 +0530

    user registration api done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
 
 
 // app config
@@ -29,6 +30,7 @@ app.use(cors())
 // api 
 app.use('/api/admin',adminRouter);
 app.use('/api/doctor',doctorRouter);
+app.use('/api/user',userRouter)
 
 app.get('/abc',(req, res)=>{
     res.send("API working")
