72cd0993c42513d6d9521a958d2c8e538d740873
Koushik <Koushikbajpayee06@gmail.com>
2025-12-25 23:56:39 +0530

    update profile with image

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken'
+import {v2 as cloudinary} from 'cloudinary'
 
 // API to register user
 const registerUser = async (req, res) =>{
@@ -82,7 +83,18 @@ const getProfile = async(req, res)=>{
 const updateProfile = async(req,res)=>{
     try{
         const {userId, name, phone, address, dob, gender} = req.body;
-        const imageFile = req.file
+        const imageFile = req.file;
+        if(!name || !phone || ! dob || !gender ){
+            return res.json({success:false, message:"Data Missing"})
+        }
+        await userModel.findByIdAndUpdate(userId, {name,phone,address:JSON.parse(address),dob,gender})
+        if(imageFile){
+            //Upload image to cloudinary 
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:'image'});
+            const imageURL = imageUpload.secure_url;
+
+            await userModel.findByIdAndUpdate(userId,{image:imageURL})
+        }
     }catch(err){
         console.log(err);
         res.json({success:false, message:err.message})
