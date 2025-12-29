05157d25349229936749d63c580d92b3a4687122
Koushik <Koushikbajpayee06@gmail.com>
2025-12-30 02:16:57 +0530

    image updated succesfully in navbar also from my profile page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,7 +5,8 @@ import { AppContext } from '../context/AppContext';
 
 const NavBar = () => {
     const navigate = useNavigate();
-    const {token, setToken} = useContext(AppContext);
+    const {token, setToken, userData} = useContext(AppContext);
+
     const [showMenu, setShowMenu] = useState(false);  
     const logout = ()=>{
       setToken(false);
@@ -35,9 +36,9 @@ const NavBar = () => {
       </ul>
       <div className='flex items-center gap-4'>
         {
-            token 
+            token  && userData
             ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-                <img className='w-8 rounded-full' src={assets.profile_pic} alt=''></img>
+                <img className='w-8 rounded-full' src={userData.image} alt=''></img>
                 <img className='w-2.5' src={assets.dropdown_icon} alt=''/>
                 <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
